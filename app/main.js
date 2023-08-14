ef9a1e1912e428074aa1b20f245505fe961fc785
sincerajn <sincerajn@outlook.com>
2023-08-14 13:38:36 +0800

    ~

--- app/main.js
+++ app/main.js
@@ -1,16 +1,15 @@
 console.time('timer')
+console.timeLog('timer')
 
 import createEnter from './modules/generators/createEnter.js'
 import print from './modules/utils/print.js'
 
 try {
-  console.timeLog('timer')
   await Promise.all([createEnter()])
-  console.timeLog('timer')
 
   print('🎉 Mission accomplished!')
 } catch (error) {
   console.error(error)
+} finally {
+  console.timeEnd('timer')
 }
-
-console.timeEnd('timer')
--- app/modules/generators/createEnter.js
+++ app/modules/generators/createEnter.js
@@ -1,20 +1,22 @@
 import fs from 'fs/promises'
-import getFileDetails from '../utils/getFileDetails.js'
 import MonaTplEngine from '../templates/MonaTplEngine.js'
+import getFileDetails from '../utils/getFileDetails.js'
 import print from '../utils/print.js'
 
 export default async function createEnter() {
   const { content: enterTpl } = await getFileDetails('./src/index.html')
   const ctx = MonaTplEngine(enterTpl)
 
-  const index = await ctx
-    .useIncludesAsync()
-    .then(ctx => ctx.useMeta().useStyle().useVars().render())
-
   try {
+    const index = await ctx
+      .useIncludesAsync()
+      .then(ctx => ctx.useMeta().useStyle().useVars().render())
+
     await fs.writeFile('./dist/index.html', index)
     print('🏠 Website entry successfully built.')
   } catch (error) {
     throw error
+  } finally {
+    console.timeLog('timer')
   }
 }
